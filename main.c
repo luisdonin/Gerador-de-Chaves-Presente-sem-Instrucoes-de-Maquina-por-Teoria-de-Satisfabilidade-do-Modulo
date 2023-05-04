34edde205518b862f2054127993ad8705f147f50
J. L. B. Donin <88415069+luisdonin@users.noreply.github.com>
2023-05-04 15:24:34 -0300

    Mudei um monte de coisa
    aosdhaoushd

--- main.c
+++ main.c
@@ -3,33 +3,38 @@
 
 
 
-int main() {
-
-        int valCode[5];
-
-        int sum;
-        int i;
-        printf("Validation code: ");
-        for( i=0; i<5;i++){
-            scanf("%d", &valCode[i]);
-        }
+int valida(int valCode[5]) {
+    int sum, i;
 
         sum = valCode[0] + valCode[1] + valCode[2] + valCode[3] + valCode[4] ;
             
 
         if(sum %2 == 0){
             printf("\nWelcome!");
+            return 1;
         }
         else{
             printf("\nInvalid Key!");
             printf("\nsum: %d\n", sum);
         }
 
-        printf("Validation: ");
+        printf("\nValidation key: ");
         for( i=0; i<5;i++){
             printf(" %d", valCode[i]);
         }
         printf("\n");
 
         return 0;
+}
+
+int main(){
+    int valCode[5];
+
+    int i;
+    printf("Validation key: ");
+    for( i=0; i<5;i++){
+        scanf("%d", &valCode[i]);
+    }
+    valida(valCode);
+    return 0;
 }
